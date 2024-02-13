67e4921db4381f203bd12e2ce83d7a85e75ecc33
HanielUlises <hanielulises2003@gmail.com>
2024-02-12 19:20:10 -0600

    Measures of central tendency

--- Descriptive statistics/Central Tendency /dynamic_statistics.h
+++ Descriptive statistics/Central Tendency /dynamic_statistics.h
@@ -2,33 +2,19 @@
 #define DYNAMIC_STATISTICS_H
 
 #include <vector>
-#include <algorithm>
-#include <numeric>
 
 template<typename T>
 class DynamicStatistics {
 public:
-    DynamicStatistics(const std::vector<T>& data) : data(data) {}
+    DynamicStatistics(const std::vector<T>& data);
 
-    T mean() const {
-        T sum = std::accumulate(data.begin(), data.end(), T(0));
-        return sum / data.size();
-    }
-
-    T median() const {
-        std::vector<T> sortedData = data;
-        std::sort(sortedData.begin(), sortedData.end());
-        size_t n = sortedData.size();
-
-        if (n % 2 == 0) {
-            return (sortedData[n / 2 - 1] + sortedData[n / 2]) / 2.0;
-        } else {
-            return sortedData[n / 2];
-        }
-    }
+    T mean() const;
+    T median() const;
 
 private:
     std::vector<T> data;
 };
+
+#include "dynamic_statistics.tpp"
 
 #endif // DYNAMIC_STATISTICS_H
