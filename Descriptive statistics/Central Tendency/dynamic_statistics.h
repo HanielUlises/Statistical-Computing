40a70abad30c7dfa376cb2e160028cb1686accbe
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2025-05-22 23:28:06 -0600

    Central tendency metrics

--- Descriptive statistics/Central Tendency/dynamic_statistics.h
+++ Descriptive statistics/Central Tendency/dynamic_statistics.h
@@ -5,33 +5,33 @@
 #include <optional>
 
 template<typename T>
-class DynamicStatistics {
+class Central_Tendency {
 public:
-    DynamicStatistics(const std::vector<T>& data);
+    Central_Tendency(const std::vector<T>& data);
 
     T mean() const;
     T median() const;
     std::optional<T> mode() const;
     T variance() const;
-    T standardDeviation() const;
+    T standard_deviation() const;
     T range() const;
     T skewness() const;
     T kurtosis() const;
     T percentile(double p) const;
-    T interquartileRange() const;
-    T covariance(const DynamicStatistics<T>& other) const;
-    T correlationCoefficient(const DynamicStatistics<T>& other) const;
+    T interquartile_range() const;
+    T covariance(const Central_Tendency<T>& other) const;
+    T correlation_coefficient(const Central_Tendency<T>& other) const;
     std::vector<T> normalize() const;
 
-    void addDataPoint(const T& value);
-    void removeDataPoint(const T& value);
-    void clearData();
+    void add_data_point(const T& value);
+    void remove_data_point(const T& value);
+    void clear_data();
 
 private:
     std::vector<T> data;
 
-    void validateNonEmpty() const;
-    void validateSize(size_t minSize) const;
+    void validate_non_empty() const;
+    void validate_size(size_t minSize) const;
 };
 
 
