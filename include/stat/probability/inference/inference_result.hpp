1b46800c5c3d09fd7b8a59edada4186930371639
HanielUlises <hanielulises2003@gmail.com>
2026-02-16 23:16:51 -0600

    Use explicit custom hasher for assignment in Factor to avoid std::hash instantiation issues

--- include/stat/probability/inference/inference_result.hpp
+++ include/stat/probability/inference/inference_result.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #include <unordered_map>
 #include <cstddef>
 
@@ -8,15 +9,39 @@
 
 namespace stat::prob::inference {
 
+using Variable = std::size_t;
+
+using Evidence = std::unordered_map<Variable, std::size_t>;
+
+struct Query {
+    std::vector<Variable> variables;
+};
 
 struct InferenceResult {
+
     using Assignment =
         stat::prob::graphical::assignment<std::size_t>;
 
-    std::unordered_map<Assignment, double> distribution;
+    using Hasher =
+        stat::core::hashing::assignment_hash<std::size_t>;
+
+    std::unordered_map<
+        Assignment,
+        double,
+        Hasher
+    > distribution;
 
     void normalize();
 };
-} // namespace stat::prob::inference
 
-#include "inference_result.inl"
+class InferenceEngine {
+public:
+    virtual ~InferenceEngine() = default;
+
+    virtual InferenceResult infer(
+        const Query& query,
+        const Evidence& evidence
+    ) const = 0;
+};
+
+} // namespace stat::prob::inference
