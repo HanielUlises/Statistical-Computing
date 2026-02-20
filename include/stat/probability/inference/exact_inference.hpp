943ad7208ff8ee71595dda74c98a4679b2add2cb
HanielUlises <hanielulises2003@gmail.com>
2026-02-19 23:40:04 -0600

    Factor retrieval

--- include/stat/probability/inference/exact_inference.hpp
+++ include/stat/probability/inference/exact_inference.hpp
@@ -7,35 +7,41 @@
 #include <stat/probability/graphical/assignment.hpp>
 #include <stat/probability/inference/inference_engine.hpp>
 #include <stat/probability/inference/inference_result.hpp>
+#include <stat/probability/graphical/bayesian_network.hpp>
 
 namespace stat::prob::inference {
 
 class ExactInference {
-public:
-    using Assignment =
-        stat::prob::graphical::assignment<std::size_t>;
+    public:
+        using Assignment =
+            stat::prob::graphical::assignment<std::size_t>;
 
-    using Factor =
-        stat::prob::graphical::Factor<Assignment>;
+        using Factor =
+            stat::prob::graphical::Factor<Assignment>;
 
-    explicit ExactInference(std::vector<Factor> factors);
+        explicit ExactInference(std::vector<Factor> factors);
 
-    InferenceResult query(
-        const std::vector<std::size_t>& query_vars,
-        const Assignment& evidence = {}
-    ) const;
+        template <typename Variable>
+        ExactInference(const BayesianNetwork<Variable>& bn)
+            : factors_(bn.factors())
+        {}
 
-private:
-    std::vector<Factor> factors_;
+        InferenceResult query(
+            const std::vector<std::size_t>& query_vars,
+            const Assignment& evidence = {}
+        ) const;
 
-    std::vector<Factor>
-    apply_evidence(const Assignment& evidence) const;
+    private:
+        std::vector<Factor> factors_;
 
-    std::vector<Factor>
-    eliminate_variable(
-        std::vector<Factor> factors,
-        std::size_t var
-    ) const;
+        std::vector<Factor>
+        apply_evidence(const Assignment& evidence) const;
+
+        std::vector<Factor>
+        eliminate_variable(
+            std::vector<Factor> factors,
+            std::size_t var
+        ) const;
 };
 
 } // namespace stat::prob::inference
