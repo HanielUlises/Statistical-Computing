11658dd8b5ca3ee922fa299e8f6209fe51298575
HanielUlises <hanielulises2003@gmail.com>
2026-06-18 23:17:56 -0600

    Running mean

--- include/stat/measures/running_mean.hpp
+++ include/stat/measures/running_mean.hpp
@@ -5,56 +5,25 @@
 namespace stat {
 
 template<typename T>
-class RunningVariance {
+class RunningMean {
 private:
     std::size_t n_ = 0;
     T mean_ = T{};
-    T m2_ = T{};
 
 public:
-
     void update(T x) {
-
         ++n_;
+        mean_ += (x - mean_) / static_cast<T>(n_);
+    }
 
-        const T delta = x - mean_;
-        mean_ += delta / static_cast<T>(n_);
-
-        const T delta2 = x - mean_;
-        m2_ += delta * delta2;
+    [[nodiscard]]
+    T value() const {
+        return mean_;
     }
 
     [[nodiscard]]
     std::size_t count() const {
         return n_;
-    }
-
-    [[nodiscard]]
-    T mean() const {
-        return mean_;
-    }
-
-    [[nodiscard]]
-    T variance() const {
-
-        if(n_ < 2)
-            return T{};
-
-        return m2_ / static_cast<T>(n_ - 1);
-    }
-
-    [[nodiscard]]
-    T population_variance() const {
-
-        if(n_ == 0)
-            return T{};
-
-        return m2_ / static_cast<T>(n_);
-    }
-
-    [[nodiscard]]
-    T standard_deviation() const {
-        return std::sqrt(variance());
     }
 };
 
